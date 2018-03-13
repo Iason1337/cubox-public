de552b1354dbb366c23489626f4b638bc3cda2fb
Iason1337 <37274350+Iason1337@users.noreply.github.com>
2018-03-13 20:10:47 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log('I am now working correctly!');
-    client.setPlayingGame("potato")
+    client.user.setActivity('Null', { type: 'PLAYING'
 });
 
 client.on('message', message => {
