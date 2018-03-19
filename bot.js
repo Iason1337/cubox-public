db7174ff282aaf7341efe4d929379770b86b8be6
Iason1337 <37274350+Iason1337@users.noreply.github.com>
2018-03-19 23:23:20 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const guildMember = message.member;
 
 client.on('ready', () => {
     console.log('I am now working correctly!');
