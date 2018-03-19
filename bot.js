247f3abc24bc38e9fbddc82961f0de323f660274
Iason1337 <37274350+Iason1337@users.noreply.github.com>
2018-03-19 21:54:07 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,16 +1,20 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const prefix = ">";
 
 client.on('ready', () => {
     console.log('I am now working correctly!');
 });
 
+client.on("message", (message) => {
+  if (!message.content.startsWith(prefix)) return;
+
 client.on('ready', () => {
-    client.user.setPresence( { game: {name: '!commands', type: 0 }});
+    client.user.setPresence( { game: {name: 'Discord', type: 0 }});
 });
 
 client.on('message', message => {
-  if (message.content === 'cubox can you give my avatar?') {
+  if (message.content.startsWith(prefix + "cubox can you give my avatar?")) {
     message.reply(message.author.avatarURL);
   }
     
@@ -26,11 +30,11 @@ client.on('message', message => {
     }
 });
 
-client.on('message', message => {
-    if (message.content === '!commands') {
-    	message.author.send('Hi!\nYou can ask cubox the following thing:\n\ncubox can you give my avatar?');
-    }
-});
+//client.on('message', message => {
+//    if (message.content === 'Test') {
+//    	message.author.send('Test');
+//    }
+//});
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
