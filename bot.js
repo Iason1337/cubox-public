03b3f33187e29614e31c312d1d8dcb5510cda711
Iason1337 <37274350+Iason1337@users.noreply.github.com>
2018-03-19 21:28:30 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,12 +17,6 @@ client.on('message', message => {
 });
 
 client.on('message', message => {
-    if (message.content === 'cubox who created you?') {
-    	message.reply('Cuboxic created me :)');
-    }
-});
-
-client.on('message', message => {
     if(message.author.bot) return;
     var re =  /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi.exec(message.cleanContent);
     if(re != null){
