32eca5da44b8df16ba13af86548a7edd326efedd
Iason1337 <37274350+Iason1337@users.noreply.github.com>
2018-03-19 22:53:50 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -25,7 +25,7 @@ client.on('message', function(message) {
         if (message.member.hasPermission("MANAGE_MESSAGES")) {
             message.channel.fetchMessages()
                .then(function(list){
-                    message.channel.bulkDelete(list);
+                    message.channel.bulkDelete(50);
                 }, function(err){message.channel.send("ERROR: ERROR CLEARING CHANNEL.")})                        
         }
     }
