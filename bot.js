a4d67f3b09d9f6932f6b740296aa449d9b5091bd
Iason1337 <37274350+Iason1337@users.noreply.github.com>
2018-03-30 13:15:34 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ client.on('ready', () => {
 });
 
 client.on('ready', () => {
-    client.user.setPresence( { game: {name: '>information | NEW UPDATE >update', type: 0 }});
+    client.user.setPresence( { game: {name: '>information | NEW UPDATE >updates', type: 0 }});
 });
 
 client.on('message', message => {
